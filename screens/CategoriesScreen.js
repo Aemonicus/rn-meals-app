a5e739f293654e7beb4bdf7eab4d6fcc47855966
Florent VICENTE <vicentegroupe@gmail.com>
2021-04-21 12:59:03 +0200

    configuring the Header with Navigation Options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,33 @@
 import React from 'react'
-import { View, Text, FlatList, StyleSheet, Button } from 'react-native'
+import { View, Text, FlatList, StyleSheet, Button, TouchableOpacity, Platform } from 'react-native'
 
 import { CATEGORIES } from '../data/dummy-data'
-
-const renderGridItem = itemData => {
-  return <View style={styles.gridItem}>
-    <Text>{itemData.item.title}</Text>
-  </View>
-}
+import Colors from "../constants/Colors"
 
 const CategoriesScreen = ({ navigation }) => {
+
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity style={styles.gridItem} onPress={() => { navigation.navigate({ routeName: "CategoryMeals" }) }}>
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
   return (
     <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   )
+}
+
+// Rappel : les fonctions sont des objets en js, après avoir créé la fonction/objet, je peux lui ajouter des propriétés. Je rajoute donc la propriété "navigationOptions" qui me permet, entre autres, d'ajouter du style au header de la barre de navigation
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
 }
 
 const styles = StyleSheet.create({
