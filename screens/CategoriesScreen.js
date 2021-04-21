aa7d79e7f700a9470bb85c152889726c1fdaaf30
Florent VICENTE <vicentegroupe@gmail.com>
2021-04-21 12:40:28 +0200

    grid of categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, FlatList, StyleSheet, Button } from 'react-native'
+
+import { CATEGORIES } from '../data/dummy-data'
+
+const renderGridItem = itemData => {
+  return <View style={styles.gridItem}>
+    <Text>{itemData.item.title}</Text>
+  </View>
+}
 
 const CategoriesScreen = ({ navigation }) => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      {/* Version alternative */}
-      {/* <Button title="Go to Meals!" onPress={() => { navigation.navigate({ routeName: "CategoryMeals" }) }} /> */}
-      <Button title="Go to Meals!" onPress={() => { navigation.navigate("CategoryMeals") }} />
-    </View>
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   )
 }
 
@@ -17,6 +20,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 })
 
