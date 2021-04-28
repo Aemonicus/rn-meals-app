217bd8e1b1c2847f859a45ce42a35493b72ca925
Florent VICENTE <vicentegroupe@gmail.com>
2021-04-28 18:42:17 +0200

    redux and store setup

--- App.js
+++ App.js
@@ -3,11 +3,22 @@ import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font'
 import { StyleSheet, Text, View } from 'react-native';
 import { enableScreens } from "react-native-screens"
+import { createStore, combineReducers } from "redux"
+import { Provider } from "react-redux"
 
 import MealsNavigator from './navigation/MealsNavigator'
+import mealsReducer from "./store/reducers/meals"
 
 // Améliore légèrement les performances des screens lors de la navigation
 enableScreens()
+
+// On récupère les reducers
+const rootReducer = combineReducers({
+  meals: mealsReducer
+})
+
+// On donne les reducers au store pour le créer
+const store = createStore(rootReducer)
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -28,7 +39,10 @@ export default function App() {
   }
 
   return (
-    <MealsNavigator />
+    // On encadre l'application avec provider qui englobera l'application avec le store
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
 
